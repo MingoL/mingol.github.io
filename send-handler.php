e4cdfe7f23bebddf2f137436aab0071cfaaf90c5
PowerSupply <sly.007.2000@gmail.com>
2023-03-25 11:40:12 +0800

    modified:   contact.html 	modified:   send-handler.php

--- send-handler.php
+++ send-handler.php
@@ -30,6 +30,7 @@ if(isset($_POST['send'])){
         //Recipients
         $mail->setFrom('Laicom@laicom.com.tw', 'Mailer');
         $mail->addAddress('mingo.lai@gmail.com', 'Mingo');     
+        $mail->addAddress('laicom@hotmail.com', 'David');
         
         //Content
         $mail->isHTML(true);
